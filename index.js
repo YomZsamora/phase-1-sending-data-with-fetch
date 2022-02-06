862bd90a916f7c038083acaf90c2526a45e2cbe7
yomZsamora <samora.y@adzumi.co.ke>
2022-02-06 19:12:02 +0300

    Data being sent in fetch() stored in the body of the configurationObject.

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 
 
-
 let submitData = (username, emailAddress) => {
 	
+	// Data being sent in fetch() must be stored in the body of the configurationObject
 	const submittedData = {
 		username: username,
 		emailAddress: emailAddress
 	};
 	
+	// The configurationObject contains three core components that are needed for standard POST requests: the HTTP verb, the headers, and the body.
 	const configurationObject = {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json",
 			Accept: "application/json",
 		},
+		// By passing an object in, JSON.stringify() will return a string.
 		body: JSON.stringify(submittedData),
 	};
 }
