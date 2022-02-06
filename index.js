daa923b61978789bbb29067b55ff218c00256c15
yomZsamora <samora.y@adzumi.co.ke>
2022-02-06 19:09:02 +0300

    Done with the configurationObject: the HTTP verb, the headers, and the body.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add your code here
+
+
+
+let submitData = (username, emailAddress) => {
+	
+	const submittedData = {
+		username: username,
+		emailAddress: emailAddress
+	};
+	
+	const configurationObject = {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			Accept: "application/json",
+		},
+		body: JSON.stringify(submittedData),
+	};
+}
