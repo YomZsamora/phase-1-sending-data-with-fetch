1b8b00b6cb13f8ffe4c9f8c157f2ca8808a00a81
yomZsamora <samora.y@adzumi.co.ke>
2022-02-06 19:25:19 +0300

    Using fetch() to send GET requests by handling responses to fetch().

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-
+const body = document.querySelector("body");
 
 let submitData = (username, emailAddress) => {
 	
 	// Data being sent in fetch() must be stored in the body of the configurationObject
 	const submittedData = {
-		username: username,
-		emailAddress: emailAddress
+		name: username,
+		email: emailAddress
 	};
 	
 	// The configurationObject contains three core components that are needed for standard POST requests: the HTTP verb, the headers, and the body.
@@ -18,4 +18,15 @@ let submitData = (username, emailAddress) => {
 		// By passing an object in, JSON.stringify() will return a string.
 		body: JSON.stringify(submittedData),
 	};
+	
+	// Using fetch() to send GET requests by handling responses to fetch()
+	return fetch("http://localhost:3000/users", configurationObject)
+	.then(resp => resp.json())
+	.then(json => {
+		body.append(json.id);
+	})
+	.catch( error => {
+		alert("Kuna Bug Mahali!");
+		body.append(error);
+	});
 }
